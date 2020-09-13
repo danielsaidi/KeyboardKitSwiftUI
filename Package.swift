2d64d75c28ff1eacb37e8d80b6ee8e5a1c83424c
Daniel Saidi <daniel.saidi@gmail.com>
2020-09-13 20:19:19 +0200

    Update Mockery to 0.6.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         //.package(url: "https://github.com/danielsaidi/KeyboardKit.git", .branch("master")),
         .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-        .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.3.0")
+        .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.6.0")
     ],
     targets: [
         .target(
