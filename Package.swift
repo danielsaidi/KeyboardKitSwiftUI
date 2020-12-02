187166bac9b8deaaab1d213ecc21233ba2e1cf97
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-03 00:27:49 +0100

    Update SPM branch

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(url: "https://github.com/danielsaidi/KeyboardKit.git", from: "3.1.1"),
-        .package(url: "https://github.com/danielsaidi/KeyboardKit.git", .branch("layout")),
+        .package(url: "https://github.com/danielsaidi/KeyboardKit.git", .branch("master")),
         .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
         .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.6.0")
