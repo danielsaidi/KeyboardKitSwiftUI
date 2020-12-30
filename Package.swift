3464605f54c38cc5104227052304cffef1c15368
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-30 23:59:00 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/danielsaidi/KeyboardKit.git", from: "3.3.0"),
         //.package(url: "https://github.com/danielsaidi/KeyboardKit.git", .branch("master")),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
-        .package(url: "https://github.com/danielsaidi/Mockery.git", from: "0.6.0")
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
+        .package(url: "https://github.com/danielsaidi/Mockery.git", .upToNextMajor(from: "0.7.0"))
     ],
     targets: [
         .target(
