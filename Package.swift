79f9eb7430e3e30429835eca376caeb6b1d69439
Daniel Saidi <daniel.saidi@gmail.com>
2021-01-07 01:50:32 +0100

    Adjust KK dependency branch

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(url: "https://github.com/danielsaidi/KeyboardKit.git", from: "3.3.0"),
-        .package(url: "https://github.com/danielsaidi/KeyboardKit.git", .branch("secondary-callout")),
+        .package(url: "https://github.com/danielsaidi/KeyboardKit.git", .branch("master")),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
         .package(url: "https://github.com/danielsaidi/MockingKit.git", .upToNextMajor(from: "0.8.0"))
